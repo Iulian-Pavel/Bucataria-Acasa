1a58358c508a84d032b72fc76cb2255899f5bbed
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2022-03-02 20:09:10 +0200

    created image slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Slider from './components/Slider/Slider';
+import SliderData from './components/Slider/SliderData';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Slider slides={SliderData}/>
     </>
   );
 }
---
+++ src/components/Slider/SliderData.js
@@ -0,0 +1,21 @@
+import image1 from './images/pexels-adonyi-gábor-1414651.jpg'
+import image2 from './images/pexels-daria-shevtsova-1095550.jpg'
+import image3 from './images/pexels-jonathan-borba-2983101.jpg'
+import image4 from './images/pexels-lisa-fotios-1279330.jpg';
+
+const SliderData = [
+    {
+        image: image1
+    },
+    {
+        image: image2
+    },
+    {
+        image: image3
+    },
+    {
+        image: image4
+    }
+]
+
+export default SliderData;
