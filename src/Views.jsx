30fbf5dc7784bd38cbd5ad4c9ec99f7f3835a998
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2022-03-06 23:30:09 +0200

    updated paths

--- src/Views.jsx
+++ src/Views.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Recipes from './Pages/Recipes/Recipes';
-import Home from './components/Home/Home';
+import Home from './Pages/Home/Home';
 
 const Views = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/home' element={ Home() }></Route>
+      <Route path='/' exact element={ Home() }></Route>
       <Route path='/recipes' element={ Recipes() }></Route>
     </Routes>
     </BrowserRouter>
