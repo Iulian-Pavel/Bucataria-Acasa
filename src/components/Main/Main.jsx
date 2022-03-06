e2c061bf7ee73cf5c6dcdef7043340b3300713a6
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2022-03-06 23:46:36 +0200

    minimal design changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import './Main.css';
 
+const Main = () => {
+  return (
+    <>
+    <section className='main'>
+        <h1>Gustul Bucatariei Italiene</h1>
+        <p>Chiar la tine acasa!</p>
+    </section>
+    </>
+  )
+}
+
+export default Main
